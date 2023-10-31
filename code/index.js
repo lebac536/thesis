b31baa84bea791b5c0854a72f499fbc086ec554f
lebac536 <lebac536@newschool.edu>
2023-10-31 17:24:57 -0400

    adding the code

---
+++ code/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const movableDiv = document.getElementById('movableDiv');
+    const imageElement = document.getElementById('image');
+    let leftPosition = 0;
+    let topPosition = 0;
+
+    document.addEventListener('keydown', (event) => {
+        console.log("Key pressed:", event.key);
+
+        const moveDistance = 10;
+
+        if (event.key === 'ArrowRight') {
+            imageElement.src = '/character_views/1x/rightView.png'; // Change to face right
+            leftPosition += moveDistance;
+        } else if (event.key === 'ArrowLeft') {
+            leftPosition -= moveDistance;
+            imageElement.src = '/character_views/1x/leftView.png'; // Change to face left
+        } else if (event.key === 'ArrowDown') {
+            topPosition += moveDistance;
+            imageElement.src = '/character_views/1x/frontView.png'; // Change to face forward
+        } else if (event.key === 'ArrowUp') {
+            topPosition -= moveDistance;
+            imageElement.src = '/character_views/1x/backView.png'; // Change to face back
+        }
+
+        movableDiv.style.left = leftPosition  + 'px';
+        movableDiv.style.top = topPosition + 'px';
+    });
+});
